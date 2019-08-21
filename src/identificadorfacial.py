1c4a8cc202b7e30cd0085c19868b90a355dc62d8
Rennan Gaio <rennan.gaio12@gmail.com>
2019-08-21 18:43:40 -0300

    first commit

---
+++ src/identificadorfacial.py
@@ -0,0 +1,13 @@
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from PIL import Image
+import glob
+import cv2
+from sklearn.model_selection import train_test_split
+# from keras.layers import Dropout, Dense
+# from keras.layers.normalization import BatchNormalization
+# from keras.models import Sequential, load_model
+# from keras.applications import VGG16
+from sklearn.metrics import accuracy_score, confusion_matrix
